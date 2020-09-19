be3e19ad3ca89793e0488a5ef7358129915ed19c
samhitabisht <68056370+samhitabisht@users.noreply.github.com>
2020-09-19 23:23:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7,isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -57,9 +57,7 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
- 	packageBody.options= {
- 		'isStatic': false
- 	}
+ 	Matter.Body.setStatic(packageBody, false)
     packageBody.velocityX= -5; 
   }
 }
